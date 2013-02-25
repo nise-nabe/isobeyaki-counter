0623b5da82c6bae5ed1e35b97179008e8554f482
nise_nabe <nise.nabe@gmail.com>
2013-02-26 02:23:36 +0900

    add build dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -12,11 +12,14 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "org.twitter4j" % "twitter4j-core" % "3.0.3"
+    "org.twitter4j" % "twitter4j-core" % "3.0.3",
+    "org.twitter4j" % "twitter4j-stream" % "3.0.3",
+    "org.fusesource.scalate" % "scalate-core_2.10" % "1.6.1"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
+    resolvers += "FuseSource Public Repository" at "http://repo.fusesource.com/nexus/content/repositories/public/"
   )
 
 }
